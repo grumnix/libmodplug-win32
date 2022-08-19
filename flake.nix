1f1e3c13c85ac605b58fddc556ff73b2b9e4eab3
Ingo Ruhnke <grumbel@gmail.com>
2022-08-19 17:42:47 +0200

    Remove flake-utils dependency

--- flake.nix
+++ flake.nix
@@ -1,17 +1,15 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05";
-    flake-utils.url = "github:numtide/flake-utils";
 
     tinycmmc.url = "github:grumbel/tinycmmc";
     tinycmmc.inputs.nixpkgs.follows = "nixpkgs";
-    tinycmmc.inputs.flake-utils.follows = "flake-utils";
 
     libmodplug_src.url = "https://downloads.sourceforge.net/project/modplug-xmms/libmodplug/0.8.9.0/libmodplug-0.8.9.0.tar.gz";
     libmodplug_src.flake = false;
   };
 
-  outputs = { self, nixpkgs, flake-utils, tinycmmc, libmodplug_src }:
+  outputs = { self, nixpkgs, tinycmmc, libmodplug_src }:
     tinycmmc.lib.eachWin32SystemWithPkgs (pkgs:
       {
         packages = rec {
